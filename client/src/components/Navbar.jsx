a630d282c9e2296462dc52cbf5324847ec3c782d
Anup Kumar <anupg2551@gmail.com>
2025-04-30 02:24:03 +0530

    Added seller toggle

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -37,38 +37,47 @@ const Navbar = () => {
         <img className="h-9" src={assets.logo} alt="logo" />
       </NavLink>
 
-      <div className="hidden sm:flex items-center gap-8">
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/products'>All Product</NavLink>
-        <NavLink to='/'>Contact</NavLink>
+        <div className="hidden sm:flex items-center gap-8">
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/products'>All Product</NavLink>
+            <NavLink to='/'>Contact</NavLink>
+            <NavLink to='/seller' className="border border-gray-300 px-3 py-1 rounded-full text-xs cursor-pointer opacity-80">Seller
+                Dashboard
+            </NavLink>
 
-        <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-          <input onChange={(e)=> setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
-         <img src={assets.search_icon} alt='search' className='w-4 h-4'/>
+            <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
+                <input onChange={(e) => setSearchQuery(e.target.value)}
+                       className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text"
+                       placeholder="Search products"/>
+                <img src={assets.search_icon} alt='search' className='w-4 h-4'/>
+            </div>
+
+            <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
+                <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80'/>
+                <button
+                    className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+            </div>
+
+            {!user ? (<button onClick={() => setShowUserLogin(true)}
+                              className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                    Login
+                </button>)
+                :
+                (
+                    <div className='relative group'>
+                        <img src={assets.profile_icon} className='w-10' alt=""/>
+                        <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
+                            <li onClick={() => navigate("my-orders")}
+                                className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders
+                            </li>
+                            <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
+                        </ul>
+                    </div>
+                )}
         </div>
 
-        <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
-          <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80'/>
-          <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
-        </div>
-
-      {!user ? ( <button onClick={()=> setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
-          Login
-        </button>)
-        :
-        (
-          <div className='relative group'>
-            <img src={assets.profile_icon} className='w-10' alt="" />
-            <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
-              <li onClick={()=> navigate("my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
-              <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
-            </ul>
-          </div>
-        )}
-      </div>
-
-<div className='flex items-center gap-6 sm:hidden'>
-      <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
+        <div className='flex items-center gap-6 sm:hidden'>
+            <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
           <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80'/>
           <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
         </div>
